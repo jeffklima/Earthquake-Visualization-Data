70fef71a959d2c1bb3f02ff011697210dbd1c6d5
jeffklima <49123604+jeffklima@users.noreply.github.com>
2019-08-17 17:10:00 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -49,71 +49,90 @@ function createFeatures(earthquakeData) {
             fillOpacity: 1
         });
         },
-        
-  });
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+// Define outdoorsmap layers
+// Logan helped!
+var outdoorsmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+maxZoom: 18,
+id: "mapbox.outdoors",
+accessToken: API_KEY
+}); 
 
-  // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
-}
+// Create our map, giving it the outdoorsmap and earthquakes layers to display on load
+var map = L.map("map", {
+center: [31.850033,-99.6500523],
+zoom: 4
+});
+
+outdoorsmap.addTo(map)
+d3.json(queryUrl, function(data){
+    L.geoJSON(data, {
+    pointToLayer: function(feature, latlng) {
+        var mag = feature.properties.mag;
+        function colorer(mag){
+            switch(true){
+                case mag <= 1: return "#ADFF2F";
+                case mag <= 2: return "#9ACD32";
+                case mag <= 3: return "#FFFF00";
+                case mag <= 4: return "#FFD700";
+                case mag <= 5: return "#9ACD32";
+                default: return "#FF0000";
+            }
+        }
+        return L.circle(latlng, {
+            radius: mag * 10000,
+            fillOpacity: 1,
+            color: colorer(mag)})
+        },
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h4>Location: " + feature.properties.place + 
+        "</h4><hr><p>Date & Time: " + new Date(feature.properties.time) + 
+        "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+        }
+    }).addTo(map);
+})
 
 function createMap(earthquakes) {
 
-  // Define outdoorsmap layers
-  var outdoorsmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.outdoors",
-    accessToken: API_KEY
-  }); 
+    // Define outdoorsmap layers
+    var outdoorsmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.outdoors",
+      accessToken: API_KEY
+    }); 
+    
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+      Earthquakes: earthquakes
+    };
   
-  // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
-
-  // Create our map, giving it the outdoorsmap and earthquakes layers to display on load
-  var myMap = L.map("map", {
-    center: [31.850033,-99.6500523],
-    zoom: 4,
-    layers: [outdoorsmap, earthquakes]
-  });
-
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
-  L.control.layers(overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-
-// Add legend to the map
-// Reference: https://gis.stackexchange.com/questions/193161/add-legend-to-leaflet-map
-  // Color function for the legend
-  function getColor(d) {
-    return d > 5 ? "#FFA500" :
-           d > 4 ? "#FFD700" :
-           d > 3 ? "#FFFF00" :
-           d > 2 ? "#9ACD32" :
-           d > 1 ? "#ADFF2F" :
-                   "#FF0000";
-  }
+    // Create our map, giving it the outdoorsmap and earthquakes layers to display on load
+    var map = L.map("map", {
+      center: [31.850033,-99.6500523],
+      zoom: 4,
+      layers: [outdoorsmap, earthquakes]
+    });
   
-  var legend = L.control({position: 'bottomright'});
-  
+  // Add legend to the map
+  // Reference: https://gis.stackexchange.com/questions/193161/add-legend-to-leaflet-map
+  var legend = L.control({ position: 'bottomright' });
+
   legend.onAdd = function (map) {
-  
       var div = L.DomUtil.create('div', 'info legend'),
-          magnitudes = [0, 1, 2, 3, 4, 5],
+          magnitude = [0, 1, 2, 3, 4, 5],
           labels = [];
-  
-      // Loop through our density intervals and generate a label with a colored square for each interval
-      for (var i = 0; i < magnitudes.length; i++) {
+
+      div.innerHTML += "<h4 style='margin:4px'>Magnitude</h4>"
+
+      for (var i = 0; i < magnitude.length; i++) {
           div.innerHTML +=
-              '<i style="background:' + getColor(magnitudes[i] + 1) + '"></i> ' +
-              magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + mmagnitudes[i + 1] + '<br>' : '+');
+              '<i style="background:' + colorer(magnitude[i] + 1) + '"></i> ' +
+              magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
       }
-  
+
       return div;
   };
-  
-  legend.addTo(myMap);
+  legend.addTo(map);
 }
