4082958c7018b3d47caf78b39204f3bea4ec4b07
jeffklima <49123604+jeffklima@users.noreply.github.com>
2019-08-20 11:03:56 -0500

    Fixed code

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,55 +1,5 @@
-// Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-// Perform a GET request to the query URL
-d3.geojson(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
-});
-
-function createFeatures(earthquakeData) {
-
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place, time, and magnitude of the earthquake
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h4>Location: " + feature.properties.place + 
-    "</h4><hr><p>Date & Time: " + new Date(feature.properties.time) + 
-    "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
-  }
-
-  // Define a function to create the circle size based on the magnitude
-  function radiusSize(magnitude) {
-    return magnitude * 20000;
-  }
-
-  // Define a function to set the circle color based on the magnitude
-  function circleColor(magnitude) {
-    if (mag <= 1) {
-        return "#ADFF2F";
-    } else if (mag <= 2) {
-        return "#9ACD32";
-    } else if (mag <= 3) {
-        return "#FFFF00";
-    } else if (mag <= 4) {
-        return "#FFD700";
-    } else if (mag <= 5) {
-        return "#FFA500";
-    } else {
-        return "#FF0000";
-    };
-  }
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
-    pointToLayer: function(earthquakeData, latlng) {
-        return L.circle(latlng, {
-            radius: radiusSize(earthquakeData.properties.mag),
-            color: circleColor(earthquakeData.properties.mag),
-            fillOpacity: 1
-        });
-        },
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 // Define outdoorsmap layers
 // Logan helped!
 var outdoorsmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -70,20 +20,20 @@ d3.json(queryUrl, function(data){
     L.geoJSON(data, {
     pointToLayer: function(feature, latlng) {
         var mag = feature.properties.mag;
-        function colorer(mag){
+        function magColors(mag){
             switch(true){
                 case mag <= 1: return "#ADFF2F";
                 case mag <= 2: return "#9ACD32";
                 case mag <= 3: return "#FFFF00";
                 case mag <= 4: return "#FFD700";
-                case mag <= 5: return "#9ACD32";
+                case mag <= 5: return "#FFA500";
                 default: return "#FF0000";
             }
         }
         return L.circle(latlng, {
             radius: mag * 10000,
             fillOpacity: 1,
-            color: colorer(mag)})
+            color: magColors(mag)})
         },
     onEachFeature: function(feature, layer) {
         layer.bindPopup("<h4>Location: " + feature.properties.place + 
@@ -91,6 +41,27 @@ d3.json(queryUrl, function(data){
         "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
         }
     }).addTo(map);
+     // Add legend to the map
+  // Reference: https://gis.stackexchange.com/questions/193161/add-legend-to-leaflet-map
+  var legend = L.control({ position: 'bottomright' });
+
+  legend.onAdd = function () {
+      var div = L.DomUtil.create('div', 'info legend'),
+          magnitude = [0, 1, 2, 3, 4, 5],
+          colors = ["#ADFF2F", "#9ACD32", "#FFFF00", "#FFD700", "#FFA500", "#FF0000"];
+
+      div.innerHTML += "<h4 style='margin:4px'>Magnitude</h4>"
+
+      for (var i = 0; i < magnitude.length; i++) {
+          div.innerHTML +=
+              "<i style='background:" + colors[i]+ "'></i> " +
+              magnitude[i] + (magnitude[i + 1] ? "&ndash;" + magnitude[i + 1] + "<br>" : "+");
+      }
+
+      return div;
+  };
+  legend.addTo(map);
+  console.log(legend)
 })
 
 function createMap(earthquakes) {
@@ -114,25 +85,4 @@ function createMap(earthquakes) {
       zoom: 4,
       layers: [outdoorsmap, earthquakes]
     });
-  
-  // Add legend to the map
-  // Reference: https://gis.stackexchange.com/questions/193161/add-legend-to-leaflet-map
-  var legend = L.control({ position: 'bottomright' });
-
-  legend.onAdd = function (map) {
-      var div = L.DomUtil.create('div', 'info legend'),
-          magnitude = [0, 1, 2, 3, 4, 5],
-          labels = [];
-
-      div.innerHTML += "<h4 style='margin:4px'>Magnitude</h4>"
-
-      for (var i = 0; i < magnitude.length; i++) {
-          div.innerHTML +=
-              '<i style="background:' + colorer(magnitude[i] + 1) + '"></i> ' +
-              magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
-      }
-
-      return div;
-  };
-  legend.addTo(map);
-}
+};
